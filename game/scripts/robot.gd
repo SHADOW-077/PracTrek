252d19b9f26ac7e08e45ba66a4c91a55a462cf5b
shadow <144371189+SHADOW-077@users.noreply.github.com>
2024-08-25 23:54:22 +0530

    update

--- game/scripts/robot.gd
+++ game/scripts/robot.gd
@@ -2,16 +2,16 @@ extends Area2D
 
 
 func show_dialog(new_txt):
-	$Dialog.text = "ROBOT:" + new_txt
-	$Dialog.show()
+	$dialogs/margin/Dialog.text = "ROBOT:" + new_txt
+	$dialogs.show()
 	for i in range((len(new_txt) +6)):
-		if (len(new_txt) +6) != $Dialog.visible_characters:
-			$Dialog.visible_characters += 1
+		if (len(new_txt) +6) != $dialogs/margin/Dialog.visible_characters:
+			$dialogs/margin/Dialog.visible_characters += 1
 			await get_tree().create_timer(.045).timeout
 	get_parent().find_child('Button').show()
 
 
 
 func hide_dialog():
-	$Dialog.visible = false
-	$Dialog.visible_characters = 6
+	$dialogs.hide()
+	$dialogs/margin/Dialog.visible_characters = 6
